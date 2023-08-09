428b077e62e97a684ba430865cb478443be43126
isdiscodead <isdiscodead@korea.ac.kr>
2023-08-10 07:50:01 +0900

    separate templates

--- kiosk_web/settings.py
+++ kiosk_web/settings.py
@@ -65,10 +65,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'kiosk_web.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
