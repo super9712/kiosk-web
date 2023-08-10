7ccae91b2b156edf08d999807b0fb2215c12b942
dbsrudrla <rladbsrud@korea.ac.kr>
2023-08-11 00:05:51 +0900

    add megacoffee pages

--- megacoffeeapp/urls.py
+++ megacoffeeapp/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from megacoffeeapp.views import StartTemplateView
+from megacoffeeapp.views import StartTemplateView, QuestionTemplateView, MenuTemplateView, ReceiptTemplateView, CardTemplateView, CouponTemplateView, PhoneTemplateView, BarcodeTemplateView
 
 app_name = "megacoffeeapp"
 
 urlpatterns = [
     path('start/', StartTemplateView.as_view(), name='start'),
+    path('question/', QuestionTemplateView.as_view(), name='question'),
+    path('menu/', MenuTemplateView.as_view(), name='start_2'),
+    path('receipt/', ReceiptTemplateView.as_view(), name='receipt'),
+    path('card/', CardTemplateView.as_view(), name='pay_card'),
+    path('coupon/', CouponTemplateView.as_view(), name='pay_coupon'),
+    path('phone/', PhoneTemplateView.as_view(), name='pay_phone'),
+    path('barcode/', BarcodeTemplateView.as_view(), name='pay_barcode')
 ]
