307f1e5394bd1c7cd54836c24580123529ea586b
dbsrudrla <rladbsrud@korea.ac.kr>
2023-08-10 12:04:08 +0900

    add pages

--- kiosk_web/settings.py
+++ kiosk_web/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'kiosk_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'tempaltes')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
