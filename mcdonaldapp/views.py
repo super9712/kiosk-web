255fe4318e323c9a85ec8623d4808c701e599e6f
dbsrudrla <rladbsrud@korea.ac.kr>
2023-08-13 01:35:22 +0900

    fix css

--- mcdonaldapp/views.py
+++ mcdonaldapp/views.py
@@ -4,9 +4,28 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class StartTemplateView(TemplateView):
+    template_name = 'mcdonaldapp/start.html'
+
+class SelectTemplateView(TemplateView):
     template_name = 'mcdonaldapp/index2.html'
 
 class MenuTemplateView(TemplateView):
     template_name = 'mcdonaldapp/index.html'
 
+class HowmanyTemplateView(TemplateView):
+    template_name = 'mcdonaldapp/howmany(example).html'
 
+class BasketTemplateView(TemplateView):
+    template_name = 'mcdonaldapp/basket.html'
+
+class PaymentTemplateView(TemplateView):
+    template_name = 'mcdonaldapp/index3.html'
+
+class InputcardTemplateView(TemplateView):
+    template_name = 'mcdonaldapp/inputcard.html'
+
+class InputcashTemplateView(TemplateView):
+    template_name = 'mcdonaldapp/inputcash.html'
+
+class CompletecashTemplateView(TemplateView):
+    template_name = 'mcdonaldapp/complete.html'
