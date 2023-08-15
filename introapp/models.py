31d37a9a7eae68f582a4d822087f0858aad8e5b3
junginny <wjddls021206@naver.com>
2023-08-15 13:49:15 +0900

    model

--- introapp/models.py
+++ introapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Response(models.Model):
+    brand = models.CharField()
+    question = models.ForeignKey()
+    answer = models.ManyToManyRel()
+
+class Question(models.Model):
+    text = models.CharField()
+
+class Answer(models.Model):
+    text = models.CharField()
+
+
+class Complete(models.Model):
+    accuracy = models.DecimalField()
+    totalTime = models.IntegerField()
+
