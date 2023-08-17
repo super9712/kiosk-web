2a7c924072459bada565f7d9f8b3cf54f276d568
dbsrudrla <rladbsrud@korea.ac.kr>
2023-08-17 17:46:56 +0900

    fix complete css styles

--- static/js/intro/completeResult.js
+++ static/js/intro/completeResult.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', function() {
     })
 
     document.querySelector('.order-list').innerHTML = result1;
-    document.querySelector('.method-order').innerHTML = '결제 수단 :' + orderMenu[0].method;
+    document.querySelector('.method-order').innerHTML = '결제 수단 : ' + orderMenu[0].method;
 
     // 주문 했어야 하는 내역 출력
     let result2 = '';
@@ -29,7 +29,7 @@ window.addEventListener('DOMContentLoaded', function() {
     })
 
     document.querySelector('.answer-list').innerHTML = result2;
-    document.querySelector('.method-mission').innerHTML = '결제 수단 :' + order.method;
+    document.querySelector('.method-mission').innerHTML = '결제 수단 : ' + order.method;
 
     // 내역 삭제
     sessionStorage.removeItem("clickData");
