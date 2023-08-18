57c8cf0666b78a01285116f6b21c5a200ecf30ea
yewon <ye7648@gmail.com>
2023-08-18 19:15:47 +0900

    admin

--- introapp/admin.py
+++ introapp/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from introapp.models import Question, Answer
+from .models import Question, Answer
 
 @admin.register(Question)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['text']
-    list_display_links = ['text']
 
 @admin.register(Answer)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['text']
-    list_display_links = ['text']
--- megacoffeeapp/admin.py
+++ megacoffeeapp/admin.py
@@ -1,19 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from megacoffeeapp.models import Menu, Quantity, Option
+from .models import Menu, Quantity, Option
 
 @admin.register(Menu)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['name']
-    list_display_links = ['name']
 
 @admin.register(Option)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['name']
-    list_display_links = ['name']
 
 @admin.register(Quantity)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['name']
-    list_display_links = ['name']
+    list_display = ['value']
