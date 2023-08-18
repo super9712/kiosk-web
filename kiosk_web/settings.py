321068a414a4efffaa4428bb510779515f6e8971
yewon <ye7648@gmail.com>
2023-08-18 16:11:57 +0900

    DEBUG

--- kiosk_web/settings.py
+++ kiosk_web/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
 CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
