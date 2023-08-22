b942fdfdc5e6f1f14bc88d8fa4d2b862e4c9ab14
yewon <ye7648@gmail.com>
2023-08-23 00:26:26 +0900

    pull

--- mcdonaldapp/models.py
+++ mcdonaldapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    CATEGORIES = {
+        ('single', 'Single'),
+        ('set', 'Set Menu'),
+        ('side', 'Side Menu'),
+        ('drink', 'Drink'),
+        ('dessert', 'Dessert')
+    }
+    category = models.CharField(max_length=30, default='single', choices=CATEGORIES)
+
+class Cart(models.Model):
+    menu = models.CharField(max_length=50)
+    qty = models.IntegerField()
+    price = models.IntegerField()
--- mcdonaldapp/views.py
+++ mcdonaldapp/views.py
@@ -6,30 +6,34 @@ from django.views.generic import TemplateView
 class StartTemplateView(TemplateView):
     template_name = 'mcdonaldapp/start.html'
 
+
 class SelectTemplateView(TemplateView):
-    template_name = 'mcdonaldapp/index2.html'
+    template_name = 'mcdonaldapp/here_togo.html'
+
 
 class MenuTemplateView(TemplateView):
-    template_name = 'mcdonaldapp/index.html'
+    template_name = 'mcdonaldapp/menu.html'
+
 
 class HowmanyTemplateView(TemplateView):
-    template_name = 'mcdonaldapp/howmany(example).html'
+    template_name = 'mcdonaldapp/quantity.html'
 
-
-    
 
 class BasketTemplateView(TemplateView):
     template_name = 'mcdonaldapp/basket.html'
 
+
 class PaymentTemplateView(TemplateView):
-    template_name = 'mcdonaldapp/index3.html'
+    template_name = 'mcdonaldapp/payment.html'
+
 
 class InputcardTemplateView(TemplateView):
-    template_name = 'mcdonaldapp/inputcard.html'
+    template_name = 'mcdonaldapp/insert_card.html'
+
 
 class InputcashTemplateView(TemplateView):
-    template_name = 'mcdonaldapp/inputcash.html'
+    template_name = 'mcdonaldapp/insert_cash.html'
+
 
 class CompletecashTemplateView(TemplateView):
     template_name = 'mcdonaldapp/complete.html'
-
