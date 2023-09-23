5b5599c4820ed21bc8dde959319f76606982e849
yewon <ye7648@gmail.com>
2023-09-23 14:43:35 +0900

    mc accuracy

--- static/js/intro/completeResult.js
+++ static/js/intro/completeResult.js
@@ -12,28 +12,30 @@ window.addEventListener('DOMContentLoaded', function() {
     // 시간 계산
     if ( time ) {
         console.log(time)
+        document.querySelector('.using-time').innerHTML = '걸린 시간 : ' + time + '초';
     } else {
         console.log(remain_time, paying_time);
         time = 60 - remain_time + paying_time;
         sessionStorage.setItem('time', time);
+        // 정확도 및 시간 출력
+        document.querySelector('.accuracy').innerHTML = '정확도 : ' + Math.round(accuracy) + '%';
+        document.querySelector('.using-time').innerHTML = '걸린 시간 : ' + time + '초';
+
+        // 주문한 내역 출력
+        console.log(orderMenu, order);
+        document.querySelector('.ordered-method').innerHTML = '결제 수단 : ' + orderMenu[0].method;
+        document.querySelector('.ordered-packaging').innerHTML = '포장 : ' + packaging;
+
+        let result1 = '';
+        orderMenu.map((e) => {
+            result1 += `
+            <div>
+                <div>${e.menu_name.split('/')[0]} ${e.quantity}개</div>
+                <div>${e.menu_name.split('/')[1] ? e.menu_name.split('/')[1] : '추가 옵션 없음'}</div>
+            </div>`;
+        })
+        document.querySelector('.order-list').innerHTML = result1;
     }
     
-    // 정확도 및 시간 출력
-    document.querySelector('.accuracy').innerHTML = '정확도 : ' + Math.round(accuracy) + '%';
-    document.querySelector('.using-time').innerHTML = '걸린 시간 : ' + time + '초';
-    
-    // 주문한 내역 출력
-    console.log(orderMenu, order);
-    document.querySelector('.ordered-method').innerHTML = '결제 수단 : ' + orderMenu[0].method;
-    document.querySelector('.ordered-packaging').innerHTML = '포장 : ' + packaging;
 
-    let result1 = '';
-    orderMenu.map((e) => {
-        result1 += `
-        <div>
-            <div>${e.menu_name.split('/')[0]} ${e.quantity}개</div>
-            <div>${e.menu_name.split('/')[1] ? e.menu_name.split('/')[1] : '추가 옵션 없음'}</div>
-        </div>`;
-    })
-    document.querySelector('.order-list').innerHTML = result1;
 });
